9e9b619cee784414feb0207a5254fe42a7f6a7c8
Özgür <ozgurkizilkayags.com>
2023-12-19 21:38:03 +0300

    range, enumerate

---
+++ LS11_while_iteration.py
@@ -0,0 +1,18 @@
+number = 1 
+while number <= 10:
+    print(number)
+    number += 1
+
+"""
+___________________________________________
+"""
+
+message = ''
+
+while message != 'quit':
+    message = input ('quit yazana kadar bu mesajı göreceksin')
+    print(message)
+
+"""
+___________________________________________
+"""
